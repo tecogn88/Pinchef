be2c6c654c20b020a051f9401e54213c74c5c58c
fau09 <fabian@newemage.com>
2015-03-02 13:52:46 -0700

    statics y authentication backend OK

--- pinchef/settings.py
+++ pinchef/settings.py
@@ -96,10 +96,13 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = ''
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
 
 # backends
-AUTHENTICATION_BACKENDS = (
-    'usuarios.backends.EmailBackend',
-)
+# AUTHENTICATION_BACKENDS = (
+#     'usuarios.backends.EmailBackend',
+# )
